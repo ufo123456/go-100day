cbf397fb35e0d4c797568f987c01e6d34d3561b9
ufo123456 <64509749+ufo123456@users.noreply.github.com>
2020-12-19 16:35:18 +0800

    Add files via upload

--- first1.go
+++ first1.go
@@ -1,1 +1,31 @@
+package main
 
+import "fmt"
+
+func main() {
+   var arr1 []int
+   fmt.Printf("%v - %T - 长度:%v\n",arr1,arr1,len(arr1))
+
+   var arr2 =[]int{54,12,4,6}
+   fmt.Printf("%v - %T - 长度:%v\n",arr2,arr2,len(arr2))
+
+   var arr3 = []int{1: 2,2: 4, 5: 6}
+   fmt.Printf("%v - %T - 长度:%v\n", arr3, arr3 ,len(arr3))
+   fmt.Println("--------------------------------")
+   var arr5 []int
+   fmt.Println(arr5==nil)
+   var str = []string{"php", "java", "node.js", "golang"}
+   for i := 0; i < len(str); i++ {
+      fmt.Println(str[i])
+      for k, v :=range str{
+         fmt.Println(k,v)
+         
+         
+      }
+      
+   }
+   
+
+
+
+}
